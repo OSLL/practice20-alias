c2bda39f8a017a6249ad633be257b60ecfdfbd17
Artem <artemyipeshkov@gmail.com>
2020-06-03 23:11:39 +0300

    Game Page

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/GameSettings.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/GameSettings.kt
@@ -1,11 +1,18 @@
 package com.makentoshe.androidgithubcitemplate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_game_settings.*
+import kotlinx.android.synthetic.main.activity_teams.*
 
 class GameSettings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_settings)
+        textView4.setOnClickListener {
+            val intent = Intent(this, Levels::class.java)
+            startActivity(intent)
+        }
     }
 }
