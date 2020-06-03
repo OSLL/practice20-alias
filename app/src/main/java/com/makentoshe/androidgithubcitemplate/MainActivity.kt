a8863652cf4cb0c2364c86d3a90e008866de5703
NiKiT@ <npotapov820@gmail.com>
2020-06-03 17:10:22 +0300

    First try new branch commit

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.makentoshe.androidgithubcitemplate
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        continue_button.setOnClickListener {
+            Toast.makeText(this, "Continue", Toast.LENGTH_LONG).show()
+        }
     }
 }
