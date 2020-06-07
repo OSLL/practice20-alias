6b331b10dc7ab383e043c3a1643e9962fd754ed2
Melenkaa <aa.melenka@gmail.com>
2020-06-07 15:41:00 +0300

    Add countdown during round.

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/Round.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/Round.kt
@@ -3,6 +3,10 @@ package com.makentoshe.androidgithubcitemplate
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_round.*
+import android.widget.Toast
+import android.widget.Chronometer
+import android.os.SystemClock
+import android.util.Log
 
 class Round : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,9 +17,25 @@ class Round : AppCompatActivity() {
         var settingsInfo:BooleanArray = this.intent.getBooleanArrayExtra("settingsInfo")
 
         timerCounter.setText(settingsText[1].toString())
+        chronometer.setBase((timerCounter.text.toString().toInt() * 1000).toLong())
 
         backButton.setOnClickListener {
             finish()
+        }
+
+        cardImage.setOnClickListener {
+            chronometer.start()
+            cardImage.setClickable(false)
+            chronometer.setOnChronometerTickListener {
+                var elapsedMillis: Long = SystemClock.elapsedRealtime() - chronometer.base
+                if (elapsedMillis > 1000) {
+                    timerCounter.setText((timerCounter.text.toString().toInt() - 1).toString())
+                    if (timerCounter.text.toString().toInt() == 0){
+                        chronometer.stop()
+                    }
+                    elapsedMillis = SystemClock.elapsedRealtime() - chronometer.base
+                }
+            }
         }
 
         check.setOnClickListener{
