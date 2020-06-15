ed8715308eac94334f42a526fc8cdb7d8014e9d2
Melenkaa <aa.melenka@gmail.com>
2020-06-15 16:23:48 +0300

    Add undesigned dialogs on clicking (?) button

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/GameSettings.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/GameSettings.kt
@@ -1,20 +1,33 @@
 package com.makentoshe.androidgithubcitemplate
 
 import android.content.Intent
+import android.app.Dialog
 import android.os.Bundle
 import android.widget.SeekBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_game_settings.*
 
 class GameSettings : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
 
     lateinit var list: Array<MutableList<String>>
+    lateinit var fineDialog: Dialog
+    lateinit var closeFineDialog: TextView
+    lateinit var wordDialog: Dialog
+    lateinit var closeWordDialog: TextView
+    lateinit var tasksDialog: Dialog
+    lateinit var closeTasksDialog: TextView
+    lateinit var robberyDialog: Dialog
+    lateinit var closeRobberyDialog: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_settings)
 
-
+        fineDialog = Dialog(this)
+        wordDialog = Dialog(this)
+        tasksDialog = Dialog(this)
+        robberyDialog = Dialog(this)
 
         var teams = this.intent.getStringArrayExtra("teams")
         list = Array(teams.size) { MutableList(0) { "0.0" } }
@@ -53,7 +66,21 @@ class GameSettings : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
         }
 
+        fineButton.setOnClickListener{
+            showFineDialog()
+        }
 
+        generalLastWordButton.setOnClickListener{
+            showGeneralLastWordDialog()
+        }
+
+        taskButton.setOnClickListener{
+            showTasksDialog()
+        }
+
+        robberyButton.setOnClickListener{
+            showRobberyDialog()
+        }
 
         wordsAmountSlider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
 
@@ -101,6 +128,42 @@ class GameSettings : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
     override fun finish() {
         super.finish()
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+    }
+
+    fun showFineDialog(){
+        fineDialog.setContentView(R.layout.fine_dialog)
+        closeFineDialog = fineDialog.findViewById(R.id.okButton)
+        fineDialog.show()
+        closeFineDialog.setOnClickListener{
+            fineDialog.cancel()
+        }
+    }
+
+    fun showGeneralLastWordDialog(){
+        wordDialog.setContentView(R.layout.word_dialog)
+        closeWordDialog = wordDialog.findViewById(R.id.okButton)
+        wordDialog.show()
+        closeWordDialog.setOnClickListener{
+            wordDialog.cancel()
+        }
+    }
+
+    fun showTasksDialog(){
+        tasksDialog.setContentView(R.layout.tasks_dialog)
+        closeTasksDialog = tasksDialog.findViewById(R.id.okButton)
+        tasksDialog.show()
+        closeTasksDialog.setOnClickListener{
+            tasksDialog.cancel()
+        }
+    }
+
+    fun showRobberyDialog(){
+        robberyDialog.setContentView(R.layout.robbery_dialog)
+        closeRobberyDialog = robberyDialog.findViewById(R.id.okButton)
+        robberyDialog.show()
+        closeRobberyDialog.setOnClickListener{
+            robberyDialog.cancel()
+        }
     }
 
     override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {}
